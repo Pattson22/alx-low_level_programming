45b35d801614f9dd62216e808853fb4c11fea62f
pattson22 <pattson22@gmail.com>
2023-06-15 09:03:08 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle"); return 0;
+#include <stdio.h>
+/**
+ * main - Use the function puts
+ * Return: 0 (Good)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
