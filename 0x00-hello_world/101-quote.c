2f073b4a4972259f51b44a3d37998362b89a6abc
pattson22 <pattson22@gmail.com>
2023-06-15 15:20:53 -0700

    8. UNIX is basically a simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - print exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ *   Return: 0 (Sucess)
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	Return (1);
+}
