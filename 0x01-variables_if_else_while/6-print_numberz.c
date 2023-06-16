de79ad627b3e32638dfdcae6c9b3559565c62b36
pattson22 <pattson22@gmail.com>
2023-06-16 07:18:33 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Good)
+ */
+int main(void)
+
+{
+int i;
+
+for (i = 48 ; i <= 57; i++)
+{
+putchar (i);
+}
+
+putchar ('\n');
+
+return (0);
+
+}
