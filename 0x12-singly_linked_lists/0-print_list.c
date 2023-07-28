284d52f1a43bcdc75b8e9fa21a89ad0bab84f77a
Pattson22 <pattson22@gmail.com>
2023-07-28 06:05:10 -0700

    updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,21 +1,24 @@
 #include "lists.h"
+#include <stdio.h>
 /**
- * print_list - a function that prints all the elements of a list_t list.
- * @h: singly linked list.
- * Return: the number of nodes.
+ * print_list - Prints all the elements of a list_t list.
+ * @h: A pointer to the head of the singly linked list.
+ * Return: The number of elements in the list.
  */
 size_t print_list(const list_t *h)
 {
-size_t nelem;
-nelem = 0;
-while (h != NULL)
-{
-if (h->str == NULL)
-	printf("[%d] %s\n", 0, "(nil)");
-	else
-	printf("[%d] %s\n", h->len, h->str);
-h = h->next;
-nelem++;
-}
-return (nelem);
+    size_t nelem = 0;
+
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+            printf("[%d] %s\n", 0, "(nil)");
+        else
+            printf("[%u] %s\n", h->len, h->str);
+
+        h = h->next;
+        nelem++;
+    }
+
+    return nelem;
 }
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,29 +1,41 @@
 #include "lists.h"
-/**
- * add_node - function that adds a new node at the beginning of a list_t list
- * of a list_t list.
- * @head: head of the linked list.
- * @str: string to store in the list.
- * Return: the address of the new element, or NULL if it failed.
- */
+#include <string.h>
+#include <stdlib.h>
 
+/**
+ * add_node - Adds a new node at the beginning of a list_t list.
+ * @head: A pointer to the head of the linked list.
+ * @str: The string to store in the new node.
+ *
+ * Return: The address of the new head of the list.
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new;
-size_t nchar;
+list_t *new_node;
+size_t nchar = 0;
 
-new = malloc(sizeof(list_t));
-if (new == NULL)
+/* Allocate memory for the new node */
+new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
 return (NULL);
 
-new->str = strdup(str);
+/* Duplicate the string using strdup */
+new_node->str = strdup(str);
+if (new_node->str == NULL)
+{
+free(new_node);
+return (NULL);
+}
 
-for (nchar = 0; str[nchar]; nchar++
-		;
+/* Calculate the length of the string */
+while (str[nchar])
+nchar++;
 
-new->len = nchar;
-new->next = *head;
-*head = new;
+new_node->len = nchar;
+new_node->next = *head;
 
+/* Update the head to point to the new node */
+*head = new_node;
+/* patrick's */
 return (*head);
 }
