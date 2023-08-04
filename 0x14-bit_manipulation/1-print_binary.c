7f15eff72530e5fb2180a0793787f0d70d459da4
Pattson22 <pattson22@gmail.com>
2023-08-04 16:03:26 -0700

    updated

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,7 +3,6 @@
 /**
  * print_binary - Prints the binary representation of a number.
  * @n: The number to be printed in binary format.
- * 
  * Return: Always 0
  */
 void print_binary(unsigned long int n)
